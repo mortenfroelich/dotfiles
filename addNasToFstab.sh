613006f2b3f19e667a4dbd5e4456bdd392b7a87a
Morten Frølich <mortenfroelich@users.noreply.github.com>
2019-11-03 20:28:02 +0100

    updated fstab line to be buster compatible

--- addNasToFstab.sh
+++ addNasToFstab.sh
@@ -8,5 +8,5 @@ if [ ! -d "$NASMOUNT" ]; then
 	sudo mkdir "$NASMOUNT"
 	mv creds ~/.smbcredentials
 	chmod 600 ~/.smbcredentials
-	echo "//froelichnas/morten $NASMOUNT cifs credentials=/home/morten/.smbcredentials,iocharset=utf8,sec=ntlm 0 0" | sudo tee -a /etc/fstab
+	echo "//froelichnas/morten /media/nas cifs credentials=/home/morten/.smbcredentials,x-systemd.automount,file_mode=0664,dir_mode=0775,gid=morten,uid=morten 0 0" | sudo tee -a /etc/fstab
 fi
